f45f415d588e36a3ef6aea28bbfdc504e1a40306
pngwn <hello@pngwn.io>
2021-03-28 19:12:30 +0100

    add action boilerplate

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,19 @@
-async function run() {}
+import core from "@actions/core";
+
+async function run() {
+	const target_repo = core.getInput("repo");
+	const target_branch = core.getInput("branch");
+	const cf_token = core.getInput("token");
+
+	if (target_branch !== "main" && target_branch !== "master") {
+		core.setFailed("Branch deploys are not yet supported.");
+	}
+
+	// get repo
+	// read docs in
+	// format them
+	// transform to cf format (batch keys)
+	// write to cloudflare
+}
 
 run();
