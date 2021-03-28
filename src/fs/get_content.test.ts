9fbc2323433d2a46aa704a50eb2d7ef9d87249b0
pngwn <hello@pngwn.io>
2021-03-28 17:25:20 +0100

    fs boilerplate

---
+++ src/fs/get_content.test.ts
@@ -0,0 +1,11 @@
+import * as fs from "fs/promises";
+import * as path from "path";
+
+function get_base_documentation(
+	docs_path: string,
+	working_directory: string = process.cwd()
+) {}
+function get_package_docuementation(
+	pkg_path: string,
+	working_directory: string = process.cwd()
+) {}
---
+++ src/fs/get_content.ts
@@ -0,0 +1,5 @@
+import * as fs from "fs/promises";
+import * as path from "path";
+
+function get_base_documentation(docs_path: string) {}
+function get_package_docuementation(pkg_path: string) {}
