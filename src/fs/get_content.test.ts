e56f4d89f947ccc074b6ae3705e2c1744a7b53c5
pngwn <hello@pngwn.io>
2021-03-28 18:00:26 +0100

    get base docs

--- src/fs/get_content.test.ts
+++ src/fs/get_content.test.ts
@@ -1,11 +1,24 @@
-import * as fs from "fs/promises";
+import { suite } from "uvu";
+import * as assert from "uvu/assert";
+
+import { get_base_documentation } from "./get_content";
 import * as path from "path";
 
-function get_base_documentation(
-	docs_path: string,
-	working_directory: string = process.cwd()
-) {}
-function get_package_docuementation(
-	pkg_path: string,
-	working_directory: string = process.cwd()
-) {}
+const get_docs = suite("get_base_documentation");
+const get_pkg_docs = suite("get_package_docuementation");
+
+get_docs("gets the api documentation correctly", async () => {
+	const content = await get_base_documentation(
+		"documentation",
+		path.join(__dirname, "fixtures", "repo-1")
+	);
+
+	assert.equal(content.api, [
+		["01-one.md", "file-one\n"],
+		["02-two.md", "file-two\n"],
+		["03-three.md", "file-three\n"],
+		["04-four.md", "file-four\n"],
+	]);
+});
+
+get_docs.run();
--- src/fs/get_content.ts
+++ src/fs/get_content.ts
@@ -1,5 +1,45 @@
-import * as fs from "fs/promises";
+import { promises as fs } from "fs";
 import * as path from "path";
 
-function get_base_documentation(docs_path: string) {}
-function get_package_docuementation(pkg_path: string) {}
+interface BaseDocs {
+	api: [string, string][];
+}
+
+function get_content_and_filename(
+	base: string,
+	filename: string
+): Promise<[string, string]> {
+	return new Promise(async (rs, rj) => {
+		try {
+			const content = await fs.readFile(path.join(base, filename), {
+				encoding: "utf-8",
+			});
+			rs([filename, content]);
+		} catch (e) {
+			rj(e);
+		}
+	});
+}
+
+export async function get_base_documentation(
+	docs_path: string,
+	working_directory: string = process.cwd()
+): Promise<BaseDocs> {
+	const docs_dir = path.join(working_directory, docs_path);
+
+	const api = await fs.readdir(path.join(docs_dir, "api"));
+	const api_content = await Promise.all(
+		api
+			.filter((f) => path.extname(f) === ".md")
+			.map((f) => get_content_and_filename(path.join(docs_dir, "api"), f))
+	);
+
+	return {
+		api: api_content,
+	};
+}
+
+export function get_package_docuementation(
+	pkg_path: string,
+	working_directory: string = process.cwd()
+) {}
