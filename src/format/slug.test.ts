7d628ea637a7f19625f9dcaec62aa4e1d37bd394
pngwn <hello@pngwn.io>
2021-03-28 13:00:56 +0100

    add unicode safe processor

--- src/format/slug.test.ts
+++ src/format/slug.test.ts
@@ -1,126 +1,118 @@
 import { suite } from "uvu";
 import * as assert from "uvu/assert";
 
-import { urlsafe_slug_processor } from "./slug";
+import { url_safe_processor, unicode_safe_processor } from "./slug";
 
-const url_safe = suite("urlsafe_slug_processor");
+const url_safe = suite("url_safe_processor");
+const unicode_safe = suite("unicode_safe_processor");
 
 url_safe("ascii: space separated words", () => {
-	assert.equal(urlsafe_slug_processor("Text expressions"), `Text_expressions`);
+	assert.equal(url_safe_processor("Text expressions"), `Text_expressions`);
 });
 
 url_safe("ascii: numbered text", () => {
-	assert.equal(urlsafe_slug_processor("1. export creates"), `1_export_creates`);
+	assert.equal(url_safe_processor("1. export creates"), `1_export_creates`);
 });
 
 url_safe("ascii: punctuated text", () => {
-	assert.equal(urlsafe_slug_processor("svelte.VERSION"), `svelte_VERSION`);
+	assert.equal(url_safe_processor("svelte.VERSION"), `svelte_VERSION`);
 });
 
 url_safe("ascii: text starting with the dollar sign", () => {
-	assert.equal(urlsafe_slug_processor("$destroy method"), `$destroy_method`);
+	assert.equal(url_safe_processor("$destroy method"), `$destroy_method`);
 });
 
 url_safe("ascii: numbered text containing the dollar sign", () => {
-	assert.equal(
-		urlsafe_slug_processor("1. export $destroy"),
-		`1_export_$destroy`
-	);
+	assert.equal(url_safe_processor("1. export $destroy"), `1_export_$destroy`);
 });
 
 url_safe("ascii: text containing the equal char", () => {
 	assert.equal(
-		urlsafe_slug_processor("script context=module"),
+		url_safe_processor("script context=module"),
 		`script_context_module`
 	);
 });
 
 url_safe("ascii: text containing the colon char", () => {
-	assert.equal(urlsafe_slug_processor("svelte:body"), `svelte_body`);
+	assert.equal(url_safe_processor("svelte:body"), `svelte_body`);
 });
 
 url_safe("ascii: text containing the slash char", () => {
-	assert.equal(urlsafe_slug_processor("svelte/motion"), `svelte_motion`);
+	assert.equal(url_safe_processor("svelte/motion"), `svelte_motion`);
 });
 
 url_safe("ascii: text containing the comma char", () => {
-	assert.equal(urlsafe_slug_processor("svelte, motion"), `svelte_motion`);
+	assert.equal(url_safe_processor("svelte, motion"), `svelte_motion`);
 });
 
 url_safe("unicode: should translate symbols to English", () => {
-	assert.equal(urlsafe_slug_processor("Ich ♥ Deutsch"), `Ich_love_Deutsch`);
+	assert.equal(url_safe_processor("Ich ♥ Deutsch"), `Ich_love_Deutsch`);
 });
 
 url_safe("unicode: should remove emoji", () => {
-	assert.equal(urlsafe_slug_processor("Ich 😍 Deutsch"), `Ich_Deutsch`);
+	assert.equal(url_safe_processor("Ich 😍 Deutsch"), `Ich_Deutsch`);
 });
 
 url_safe("cyrillic: space separated words", () => {
 	assert.equal(
-		urlsafe_slug_processor("Всплытие и перехват событий"),
+		url_safe_processor("Всплытие и перехват событий"),
 		`Vsplytie_i_perehvat_sobytij`
 	);
 });
 
 url_safe("cyrillic: numbered text", () => {
 	assert.equal(
-		urlsafe_slug_processor("1 Всплытие и перехват событий"),
+		url_safe_processor("1 Всплытие и перехват событий"),
 		`1_Vsplytie_i_perehvat_sobytij`
 	);
 });
 
 url_safe("cyrillic: punctuated text", () => {
 	assert.equal(
-		urlsafe_slug_processor(".Всплытие.и.перехват событий"),
+		url_safe_processor(".Всплытие.и.перехват событий"),
 		`Vsplytie_i_perehvat_sobytij`
 	);
 });
 
 url_safe("cyrillic: text starting with the dollar sign", () => {
 	assert.equal(
-		urlsafe_slug_processor("$Всплытие $ перехват событий"),
+		url_safe_processor("$Всплытие $ перехват событий"),
 		`$Vsplytie_$_perehvat_sobytij`
 	);
 });
 
 url_safe("cyrillic: text containing the dollar sign", () => {
-	assert.equal(
-		urlsafe_slug_processor("Всплытие$перехват"),
-		`Vsplytie$perehvat`
-	);
+	assert.equal(url_safe_processor("Всплытие$перехват"), `Vsplytie$perehvat`);
 });
 
 url_safe("cyrillic: text containing the equal char", () => {
 	assert.equal(
-		urlsafe_slug_processor("Всплытие = перехват=событий"),
+		url_safe_processor("Всплытие = перехват=событий"),
 		`Vsplytie_perehvat_sobytij`
 	);
 });
 
 url_safe("cyrillic: text containing the colon char", () => {
 	assert.equal(
-		urlsafe_slug_processor("Всплытие : перехват:событий"),
+		url_safe_processor("Всплытие : перехват:событий"),
 		`Vsplytie_perehvat_sobytij`
 	);
 });
 
 url_safe("cyrillic: text containing the slash char", () => {
 	assert.equal(
-		urlsafe_slug_processor("Всплытие / перехват/событий"),
+		url_safe_processor("Всплытие / перехват/событий"),
 		`Vsplytie_perehvat_sobytij`
 	);
 });
 
 url_safe("cyrillic: text containing the comma char", () => {
-	assert.equal(
-		urlsafe_slug_processor("Всплытие, перехват"),
-		`Vsplytie_perehvat`
-	);
+	assert.equal(url_safe_processor("Всплытие, перехват"), `Vsplytie_perehvat`);
 });
 
 url_safe("ascii + cyrillic: space separated words", () => {
 	assert.equal(
-		urlsafe_slug_processor("Всплытие и export перехват событий"),
+		url_safe_processor("Всплытие и export перехват событий"),
 		`Vsplytie_i_export_perehvat_sobytij`
 	);
 });
@@ -128,27 +120,27 @@ url_safe("ascii + cyrillic: space separated words", () => {
 url_safe(
 	"ascii + cyrillic: ascii word concatenated to a cyricllic word",
 	() => {
-		assert.equal(urlsafe_slug_processor("exportВсплытие"), "exportVsplytie");
+		assert.equal(url_safe_processor("exportВсплытие"), "exportVsplytie");
 	}
 );
 
 url_safe(
 	"ascii + cyrillic: cyricllic word concatenated to an ascii word",
 	() => {
-		assert.equal(urlsafe_slug_processor("Всплытиеexport"), `Vsplytieexport`);
+		assert.equal(url_safe_processor("Всплытиеexport"), `Vsplytieexport`);
 	}
 );
 
 url_safe("ascii + cyrillic: numbered text", () => {
 	assert.equal(
-		urlsafe_slug_processor("1 export Всплытие и перехват событий"),
+		url_safe_processor("1 export Всплытие и перехват событий"),
 		`1_export_Vsplytie_i_perehvat_sobytij`
 	);
 });
 
 url_safe("ascii + cyrillic: punctuated text", () => {
 	assert.equal(
-		urlsafe_slug_processor(".Всплытие.export.и.перехват событий"),
+		url_safe_processor(".Всплытие.export.и.перехват событий"),
 		`Vsplytie_export_i_perehvat_sobytij`
 	);
 });
@@ -157,7 +149,7 @@ url_safe(
 	"ascii + cyrillic: text starting with the dollar sign, followed by ascii char",
 	() => {
 		assert.equal(
-			urlsafe_slug_processor("$exportВсплытие перехват событий"),
+			url_safe_processor("$exportВсплытие перехват событий"),
 			`$exportVsplytie_perehvat_sobytij`
 		);
 	}
@@ -167,7 +159,7 @@ url_safe(
 	"ascii + cyrillic: text starting with the dollar sign, followed by unicode char",
 	() => {
 		assert.equal(
-			urlsafe_slug_processor("$Всплытие export перехват событий"),
+			url_safe_processor("$Всплытие export перехват событий"),
 			`$Vsplytie_export_perehvat_sobytij`
 		);
 	}
@@ -177,7 +169,7 @@ url_safe(
 	"ascii + cyrillic: text containing the dollar sign, followed by ascii char",
 	() => {
 		assert.equal(
-			urlsafe_slug_processor(
+			url_safe_processor(
 				"export $destroy a component prop Всплытие и перехват событий"
 			),
 			`export_$destroy_a_component_prop_Vsplytie_i_perehvat_sobytij`
@@ -189,7 +181,7 @@ url_safe(
 	"ascii + cyrillic: text containing the dollar sign, followed by unicode char",
 	() => {
 		assert.equal(
-			urlsafe_slug_processor(
+			url_safe_processor(
 				"Всплытие export $Всплытие a component prop Всплытие и перехват событий"
 			),
 			`Vsplytie_export_$Vsplytie_a_component_prop_Vsplytie_i_perehvat_sobytij`
@@ -199,26 +191,244 @@ url_safe(
 
 url_safe("ascii + cyrillic: text containing the equal char", () => {
 	assert.equal(
-		urlsafe_slug_processor("script context=module Всплытие=и перехват событий"),
+		url_safe_processor("script context=module Всплытие=и перехват событий"),
 		`script_context_module_Vsplytie_i_perehvat_sobytij`
 	);
 });
 
 url_safe("ascii + cyrillic: text containing the colon char", () => {});
 assert.equal(
-	urlsafe_slug_processor("svelte:body Всплытие и:перехват событий"),
+	url_safe_processor("svelte:body Всплытие и:перехват событий"),
 	`svelte_body_Vsplytie_i_perehvat_sobytij`
 );
 
 url_safe("ascii + cyrillic: text containing the slash char", () => {
 	assert.equal(
-		urlsafe_slug_processor("svelte/motion Всплытие и / перехват/событий"),
+		url_safe_processor("svelte/motion Всплытие и / перехват/событий"),
 		`svelte_motion_Vsplytie_i_perehvat_sobytij`
 	);
 });
 
 url_safe("ascii + cyrillic: text containing the comma char", () => {
-	assert.equal(urlsafe_slug_processor("Всплытие, export"), `Vsplytie_export`);
+	assert.equal(url_safe_processor("Всплытие, export"), `Vsplytie_export`);
+});
+
+unicode_safe("ascii: space separated words", () => {
+	assert.equal(unicode_safe_processor("Text expressions"), `Text_expressions`);
+});
+
+unicode_safe("ascii: numbered text", () => {
+	assert.equal(unicode_safe_processor("1. export creates"), `1_export_creates`);
+});
+
+unicode_safe("ascii: punctuated text", () => {
+	assert.equal(unicode_safe_processor("svelte.VERSION"), `svelte_VERSION`);
+});
+
+unicode_safe("ascii: text starting with the dollar sign", () => {
+	assert.equal(unicode_safe_processor("$destroy method"), `$destroy_method`);
+});
+
+unicode_safe("ascii: numbered text containing the dollar sign", () => {
+	assert.equal(
+		unicode_safe_processor("1. export $destroy"),
+		`1_export_$destroy`
+	);
+});
+
+unicode_safe("ascii: text containing the equal char", () => {
+	assert.equal(
+		unicode_safe_processor("script context=module"),
+		`script_context_module`
+	);
+});
+
+unicode_safe("ascii: text containing the colon char", () => {
+	assert.equal(unicode_safe_processor("svelte:body"), `svelte_body`);
+});
+
+unicode_safe("ascii: text containing the slash char", () => {
+	assert.equal(unicode_safe_processor("svelte/motion"), `svelte_motion`);
+});
+
+unicode_safe("ascii: text containing the comma char", () => {
+	assert.equal(unicode_safe_processor("svelte, motion"), `svelte_motion`);
+});
+
+unicode_safe("unicode: should preserve symbols", () => {
+	assert.equal(unicode_safe_processor("Ich ♥ Deutsch"), `Ich_love_Deutsch`);
+});
+
+unicode_safe("unicode: should remove emoji", () => {
+	assert.equal(unicode_safe_processor("Ich 😍 Deutsch"), `Ich_Deutsch`);
+});
+
+unicode_safe("cyricllic: space separated words", () => {
+	assert.equal(
+		unicode_safe_processor("Всплытие и перехват событий"),
+		`Всплытие_и_перехват_событий`
+	);
+});
+
+unicode_safe("cyricllic: numbered text", () => {
+	assert.equal(
+		unicode_safe_processor("1 Всплытие и перехват событий"),
+		`1_Всплытие_и_перехват_событий`
+	);
+});
+
+unicode_safe("cyricllic: punctuated text", () => {
+	assert.equal(
+		unicode_safe_processor(".Всплытие.и.перехват событий"),
+		`Всплытие_и_перехват_событий`
+	);
+});
+
+unicode_safe("cyricllic: text starting with the dollar sign", () => {
+	assert.equal(
+		unicode_safe_processor("$Всплытие $ перехват событий"),
+		`$_Всплытие_$_перехват_событий`
+	);
+});
+
+unicode_safe("cyricllic: text containing the dollar sign", () => {
+	assert.equal(
+		unicode_safe_processor("Всплытие$перехват"),
+		`Всплытие_$_перехват`
+	);
+});
+
+unicode_safe("cyricllic: text containing the equal char", () => {
+	assert.equal(
+		unicode_safe_processor("Всплытие = перехват=событий"),
+		`Всплытие_перехват_событий`
+	);
+});
+
+unicode_safe("cyricllic: text containing the colon char", () => {
+	assert.equal(
+		unicode_safe_processor("Всплытие : перехват:событий"),
+		`Всплытие_перехват_событий`
+	);
+});
+
+unicode_safe("cyricllic: text containing the slash char", () => {
+	assert.equal(
+		unicode_safe_processor("Всплытие / перехват/событий"),
+		`Всплытие_перехват_событий`
+	);
+});
+
+unicode_safe("cyricllic: text containing the comma char", () => {
+	assert.equal(
+		unicode_safe_processor("Всплытие, перехват"),
+		`Всплытие_перехват`
+	);
+});
+
+unicode_safe("ascii + cyricllic: space separated words", () => {
+	assert.equal(
+		unicode_safe_processor("Всплытие и export перехват событий"),
+		`Всплытие_и_export_перехват_событий`
+	);
+});
+
+unicode_safe(
+	"ascii + cyricllic: ascii word concatenated to a cyricllic word",
+	() => {
+		assert.equal(unicode_safe_processor("exportВсплытие"), `export_Всплытие`);
+	}
+);
+
+unicode_safe(
+	"ascii + cyricllic: cyricllic word concatenated to an ascii word",
+	() => {
+		assert.equal(unicode_safe_processor("Всплытиеexport"), `Всплытие_export`);
+	}
+);
+
+unicode_safe("ascii + cyricllic: numbered text", () => {
+	assert.equal(
+		unicode_safe_processor("1 export Всплытие и перехват событий"),
+		`1_export_Всплытие_и_перехват_событий`
+	);
+});
+
+unicode_safe("ascii + cyricllic: punctuated text", () => {
+	assert.equal(
+		unicode_safe_processor(".Всплытие.export.и.перехват событий"),
+		`Всплытие_export_и_перехват_событий`
+	);
+});
+
+unicode_safe(
+	"ascii + cyricllic: text starting with the dollar sign, followed by ascii char",
+	() => {
+		assert.equal(
+			unicode_safe_processor("$exportВсплытие перехват событий"),
+			`$export_Всплытие_перехват_событий`
+		);
+	}
+);
+
+unicode_safe(
+	"ascii + cyricllic: text starting with the dollar sign, followed by unicode char",
+	() => {
+		assert.equal(
+			unicode_safe_processor("$Всплытие export перехват событий"),
+			`$_Всплытие_export_перехват_событий`
+		);
+	}
+);
+
+unicode_safe(
+	"ascii + cyricllic: text containing the dollar sign, followed by ascii char",
+	() => {
+		assert.equal(
+			unicode_safe_processor(
+				"export $destroy a component prop Всплытие и перехват событий"
+			),
+			`export_$destroy_a_component_prop_Всплытие_и_перехват_событий`
+		);
+	}
+);
+
+unicode_safe(
+	"ascii + cyricllic: text containing the dollar sign, followed by unicode char",
+	() => {
+		assert.equal(
+			unicode_safe_processor(
+				"Всплытие export $Всплытие a component prop Всплытие и перехват событий"
+			),
+			`Всплытие_export_$_Всплытие_a_component_prop_Всплытие_и_перехват_событий`
+		);
+	}
+);
+
+unicode_safe("ascii + cyricllic: text containing the equal char", () => {
+	assert.equal(
+		unicode_safe_processor("script context=module Всплытие=и перехват событий"),
+		`script_context_module_Всплытие_и_перехват_событий`
+	);
+});
+
+unicode_safe("ascii + cyricllic: ext containing the colon char", () => {
+	assert.equal(
+		unicode_safe_processor("svelte:body Всплытие и:перехват событий"),
+		`svelte_body_Всплытие_и_перехват_событий`
+	);
+});
+
+unicode_safe("ascii + cyricllic: text containing the slash char", () => {
+	assert.equal(
+		unicode_safe_processor("svelte/motion Всплытие и / перехват/событий"),
+		`svelte_motion_Всплытие_и_перехват_событий`
+	);
+});
+
+unicode_safe("ascii + cyricllic: text containing the comma char", () => {
+	assert.equal(unicode_safe_processor("Всплытие, export"), `Всплытие_export`);
 });
 
 url_safe.run();
+unicode_safe.run();
--- src/format/slug.ts
+++ src/format/slug.ts
@@ -6,10 +6,20 @@ interface ProcessorOptions {
 	separator: string;
 }
 
-export const urlsafe_slug_processor = (
+interface Chunk {
+	type: "pass" | "process";
+	string: string;
+}
+
+interface ProcessedParts {
+	chunks: Chunk[];
+	current: Chunk;
+}
+
+export function url_safe_processor(
 	url: string,
 	opts?: ProcessorOptions
-): string => {
+): string {
 	const { separator = SLUG_SEPARATOR } = opts || {};
 
 	return slugify(url, {
@@ -24,4 +34,52 @@ export const urlsafe_slug_processor = (
 	})
 		.replace(/DOLLAR/g, "$")
 		.replace(/DASH/g, "-");
-};
+}
+
+const alphaNumRegex = /[a-zA-Z0-9]/;
+const unicodeRegex = /\p{Letter}/u;
+
+const isNonAlphaNumUnicode = (string: string) =>
+	!alphaNumRegex.test(string) && unicodeRegex.test(string);
+
+export function unicode_safe_processor(
+	url: string,
+	opts?: ProcessorOptions
+): string {
+	const { separator = SLUG_SEPARATOR } = opts || {};
+
+	return url
+		.split("")
+		.reduce(
+			(accum, char, index, array) => {
+				const type = isNonAlphaNumUnicode(char) ? "pass" : "process";
+
+				if (index === 0) {
+					accum.current = { type, string: char };
+				} else if (type === accum.current.type) {
+					accum.current.string += char;
+				} else {
+					accum.chunks.push(accum.current);
+					accum.current = { type, string: char };
+				}
+
+				if (index === array.length - 1) {
+					accum.chunks.push(accum.current);
+				}
+				console.log(accum);
+				return accum;
+			},
+			{ chunks: [], current: { type: "process", string: "" } } as ProcessedParts
+		)
+		.chunks.reduce((accum, chunk) => {
+			const processed =
+				chunk.type === "process"
+					? url_safe_processor(chunk.string)
+					: chunk.string;
+
+			processed.length > 0 && accum.push(processed);
+
+			return accum;
+		}, [] as string[])
+		.join(separator);
+}
